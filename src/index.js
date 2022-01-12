e9971eb405dc7a86e65e0cfeb897f29c6c566a9d
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-01-12 16:53:32 +0300

    add new files for programm

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import parse from './parsers.js';
+import format from './formatters/index.js';
+import buildTree from './treeGenerate.js';
+
+const readFile = (filename) => fs.readFileSync(path.resolve(process.cwd(), filename.trim()), 'utf-8');
+const extractFormat = (filename) => path.extname(filename).slice(1);
+
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const file1format = extractFormat(filepath1);
+  const file2format = extractFormat(filepath2);
+  const fileContent1 = readFile(filepath1);
+  const fileContent2 = readFile(filepath2);
+  const data1 = parse(file1format, fileContent1);
+  const data2 = parse(file2format, fileContent2);
+  const innerTree = buildTree(data1, data2);
+  return format(innerTree, formatName);
+};
+export default genDiff;
